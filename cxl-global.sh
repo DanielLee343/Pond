31b934d2de38296783c3836d471b76ac6eab01cc
Chameleon Cloud User <cc@nu-ins-1.novalocal>
2023-01-10 23:52:11 +0000

    fix

--- cxl-global.sh
+++ cxl-global.sh
@@ -5,7 +5,7 @@
 # Huaicheng Li <lhcwhu@gmail.com>
 #
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo "$DIR/run-globals.sh"
+source $DIR/run-globals.sh
 
 #-------------------------------------------------------------------------------
 
